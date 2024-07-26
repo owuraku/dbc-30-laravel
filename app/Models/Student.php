7248247f22b7c69d14e327830b0c18205d282211
Owuraku <boatseth3@gmail.com>
2024-07-26 14:58:21 +0000

    fixed student relationship

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
     use HasFactory;
 
 
-    public function course(): HasOne {
-        return $this->hasOne(Course::class);
+    public function course(): BelongsTo {
+        return $this->belongsTo(Course::class);
     }
 }
